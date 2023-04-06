51ab717455e0e7c52fab1a82e1783a5d63be2dd2
Akatosh1 <44877271+Akatosh1@users.noreply.github.com>
2023-04-06 15:36:56 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,5 +12,5 @@ def solution(p: float, x: np.array) -> tuple:
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
     loc = x.mean()
-    return ((loc - 0.092) / norm.ppf(alpha)) + 0.092, \
+    return ((loc - 0.092) / (1 - norm.ppf(alpha))) + 0.092, \
            ((loc - 0.092) / norm.ppf(1-alpha)) + 0.092
